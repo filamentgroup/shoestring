98bfaf9ac3b1058af29728dc51852cb31f62db76
scottjehl <scott@scottjehl.com>
2012-04-08 23:05:20 +0530

    very simple live event delegation handler

--- extensions/wrap.events.js
+++ extensions/wrap.events.js
@@ -13,5 +13,28 @@
 			}
 		});
 	};
+	
+	
+	wrap.fn.live = function( evt, callback ){
+
+		return this.each(function(){
+			
+			var self = this;
+		
+			function newCB( event ){
+				if( event.target === self ){
+					callback.apply( self, arguments );
+				}
+			}
+			
+			if( "addEventListener" in document ){
+				document.addEventListener( evt, newCB, false );
+			}
+			else if( document.attachEvent ){
+				document.attachEvent( "on" + evt, newCB );
+			}
+		});
+	};
+	
 
 })();
