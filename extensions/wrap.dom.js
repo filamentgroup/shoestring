d3c76de7d522a852c3ee605cd8ca3366e1ad341c
scottjehl <scott@scottjehl.com>
2012-04-09 13:18:05 +0530

    added a filter method

--- extensions/wrap.dom.js
+++ extensions/wrap.dom.js
@@ -187,6 +187,18 @@
 		return wrap( ret );
 	};
 	
+	wrap.fn.filter = function( sel ){
+		var ret = [],
+			sel = wrap( sel );
+		this.each(function( i ){
+			if( wrap.inArray( sel, this ) ){
+				ret.push( this );				
+			}
+		});
+		return wrap( ret );
+	};
+	
+	
 	
 	
 	
