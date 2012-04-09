b5b84244fcf97671da5a3501c916ee4f5cb4d086
scottjehl <scott@scottjehl.com>
2012-04-09 13:07:45 +0530

    added replaceWith method

--- extensions/wrap.dom.js
+++ extensions/wrap.dom.js
@@ -171,6 +171,23 @@
 		});
 	};
 	
+	wrap.fn.replaceWith = function( frag ){
+		if( typeof( frag ) === "string" ){
+			frag = wrap( frag );
+		}
+		var ret = [];
+		this.each(function( i ){
+			for( var j = 0, jl = frag.length; j < jl; j++ ){
+				var insertEl = i > 0 ? frag[ j ].cloneNode( true ) : frag[ j ];
+				this.parentNode.insertBefore( insertEl, this );
+				insertEl.parentNode.removeChild( this );
+				ret.push( insertEl );
+			}
+		});
+		return wrap( ret );
+	};
+	
+	
 	
 	
 
