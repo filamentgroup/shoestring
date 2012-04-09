fbaac629d75b079ab49e49174d3e6218b9df07aa
scottjehl <scott@scottjehl.com>
2012-04-09 11:55:25 +0530

    added misc extensions file, so far containing a simple data get/set.

---
+++ extensions/wrap.misc.js
@@ -0,0 +1,21 @@
+// Extensions
+
+// keep this wrapper around the ones you use!
+(function( undefined ){
+
+	wrap.fn.data = function( name, val ){
+		if( val !== undefined ){
+			return this.each(function(){
+				if( !this.wrapData ){
+					this.wrapData = {};
+				}
+				this.wrapData[ name ] = val;
+			});
+		}
+		else {
+			return this[ 0 ].wrapData && this[ 0 ].wrapData[ name ];
+		}
+	};
+	
+
+})();
