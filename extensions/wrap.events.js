48764d93a0ef1cd4f4c59131446c96f55f0ac331
scottjehl <scott@scottjehl.com>
2012-04-09 13:00:32 +0530

    added a trigger method and modified bind to support it

--- extensions/wrap.events.js
+++ extensions/wrap.events.js
@@ -5,15 +5,31 @@
 
 	wrap.fn.bind = function( evt, callback ){
 		return this.each(function(){
+			
+			var cb = function( e ){
+				callback.apply( this, [ e ].concat( e.args || [] )  );
+			};
+			
 			if( "addEventListener" in this ){
-				this.addEventListener( evt, callback, false );
+				this.addEventListener( evt, cb, false );
 			}
 			else if( this.attachEvent ){
-				this.attachEvent( "on" + evt, callback );
+				this.attachEvent( "on" + evt, cb );
 			}
 		});
 	};
 	
+	wrap.fn.trigger = function( evt, args ){
+		return this.each(function(){
+			// TODO needs IE8 support
+			if( document.createEvent ){
+				var event = document.createEvent( "Event" );
+				event.initEvent( evt, true, true );
+				event.args = args;
+				this.dispatchEvent( event );
+			}
+		});
+	};
 	
 	wrap.fn.live = function( evt, callback ){
 
