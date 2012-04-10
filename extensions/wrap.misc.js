96c017cb3e166c985e3d9856673fd6a00bdb72a3
scottjehl <scott@scottjehl.com>
2012-04-10 15:52:20 +0530

    fixed up data method and added removeData method

--- extensions/wrap.misc.js
+++ extensions/wrap.misc.js
@@ -4,18 +4,32 @@
 (function( undefined ){
 
 	wrap.fn.data = function( name, val ){
-		if( val !== undefined ){
-			return this.each(function(){
-				if( !this.wrapData ){
-					this.wrapData = {};
-				}
-				this.wrapData[ name ] = val;
-			});
+		if( name !== undefined ){
+			if( val !== undefined ){
+				return this.each(function(){
+					if( !this.wrapData ){
+						this.wrapData = {};
+					}
+					this.wrapData[ name ] = val;
+				});
+			}
+			else {
+				return this[ 0 ].wrapData && this[ 0 ].wrapData[ name ];
+			}
 		}
 		else {
-			return this[ 0 ].wrapData && this[ 0 ].wrapData[ name ];
+			return this[ 0 ].wrapData;
 		}
 	};
-	
 
+	wrap.fn.removeData = function( name ){
+			return this.each(function(){
+				if( name !== undefined && this.wrapData ){
+					delete this.wrapData[ name ];
+				}
+				else {
+					this[ 0 ].wrapData = {};
+				}
+			});
+	};
 })();
