8de5ba2fb5aed0bb3fb5141a4969451a01e5b8e2
scottjehl <scott@scottjehl.com>
2012-04-10 14:39:33 +0530

    added core method definition tests

---
+++ test/unit/core.js
@@ -0,0 +1,29 @@
+/*
+wrap unit tests - using qUnit
+*/
+(function(undefined){
+test( 'API Properties: wrap is defined', function() { 
+	ok( wrap !== undefined );
+});
+
+test( 'API Properties: wrap.qualified is defined', function() { 
+	ok( wrap.qualified !== undefined );
+});
+
+test( 'API Properties: wrap.qualify is defined', function() { 
+	ok( wrap.qualify !== undefined );
+});
+
+test( 'API Properties: wrap.extend is defined', function() { 
+	ok( wrap.extend !== undefined );
+});
+
+test( 'API Properties: wrap.inArray is defined', function() { 
+	ok( wrap.inArray !== undefined );
+});
+
+test( 'API Properties: wrap.ready is defined', function() { 
+	ok( wrap.ready !== undefined );
+});
+
+})();
--- test/unit/tests.js
+++
@@ -1,7 +0,0 @@
-/*
-wrap unit tests - using qUnit
-*/
-
-test( 'API Properties: wrap is defined', function() { 
-	ok( wrap );
-});
