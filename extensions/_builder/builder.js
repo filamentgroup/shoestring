82e1452094570431eaa668606cf27081cbda5a7e
scottjehl <scott@scottjehl.com>
2012-04-12 08:37:54 +0530

    added a concat all link

--- extensions/_builder/builder.js
+++ extensions/_builder/builder.js
@@ -7,34 +7,46 @@
 	var app = win.app;
 	
 	wrap(function(){
+		
+		var tarea = wrap( "textarea" )[ 0 ];
+		
+		function success(){
+			var aj = document.createElement( "script" );
+			
+			aj.src = '../wrap.ajax.ajax.js';
+			
+			aj.onload = function(){
+				wrap.ajax( "quickconcat.php?files=" + app.jsToLoad.join(",") , 
+				{
+					success: function( txt ){
+						tarea.value = txt;
+						tarea.className = "done";
+						tarea.setAttribute("readonly", true);
+						tarea.parentNode.removeChild( tarea.nextElementSibling );
+						tarea.previousElementSibling.innerHTML = "Success! Here's your custom Wrap build!";
+					}
+				});
+			};
+			
+			wrap( "head" )[0].appendChild( aj );
+		}
+		
+		wrap( "#all" )[ 0 ].addEventListener( "click", function( e ){
+			e.preventDefault();
+			for( var i in app.files.js ){
+				app.addFile( "../extensions/" + app.files.js[i] );
+			}
+			success();
+			this.parentNode.removeChild( this )
+		});
+		
 		// on form submit
 		wrap( "form" )[ 0 ].addEventListener( "submit", function( e ){
 			e.preventDefault();
 			var self= this,
-				tarea = wrap( this ).find( "textarea" )[ 0 ],
 				text = tarea.value;
 		
-			function success(){
-				var aj = document.createElement( "script" );
 			
-				aj.src = '../wrap.ajax.ajax.js';
-			
-				aj.onload = function(){
-					wrap.ajax( "quickconcat.php?files=" + app.jsToLoad.join(",") , 
-					{
-						success: function( txt ){
-							tarea.value = txt;
-							tarea.className = "done";
-							tarea.setAttribute("readonly", true);
-							tarea.parentNode.removeChild( tarea.nextElementSibling );
-							 tarea.previousElementSibling.innerHTML = "Success! Here's your custom Wrap build!";
-						}
-					});
-				};
-			
-				wrap( "head" )[0].appendChild( aj );
-			
-			}
 		
 			function attempt(){
 				try {
