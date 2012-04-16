dc698afee9d84ff4e82c15bb736a9b0fa8ad7af3
scottjehl <scott@scottjehl.com>
2012-04-16 15:19:32 +0530

    borrowed a subset of the property nomalization from jQuery core

--- extensions/wrap.dom.prop.js
+++ extensions/wrap.dom.prop.js
@@ -1,6 +1,7 @@
 // Extensions
 (function( undefined ){
 	wrap.fn.prop = function( name, val ){
+		var name = wrap.propFix[ name ] || name;
 		if( val !== undefined ){
 			return this.each(function(){
 				this[ name ] = val;
@@ -10,4 +11,13 @@
 			return this[ 0 ][ name ];
 		}
 	};
+	
+	// Property normalization, a subset taken from jQuery src
+	wrap.propFix = {
+		"class": "className",
+		contenteditable: "contentEditable",
+		"for": "htmlFor",
+		readonly: "readOnly",
+		tabindex: "tabIndex"
+	}
 })();
--- extensions/wrap.dom.removeProp.js
+++ extensions/wrap.dom.removeProp.js
@@ -1,6 +1,7 @@
 // Extensions
 (function( undefined ){
 	wrap.fn.removeProp = function( prop ){
+		var name = wrap.propFix && wrap.propFix[ name ] || name;
 		return this.each(function(){
 			this[ prop ] = undefined;
 			delete this[ prop ];
