1dd0ac4c9f8c76ed166942e0e318a1e8c5b067d7
Scott Jehl <scott@scottjehl.com>
2012-07-25 15:43:50 -0400

    improved className find/qualification

--- src/extensions/shoestring.dom.addClass.js
+++ src/extensions/shoestring.dom.addClass.js
@@ -4,7 +4,7 @@
 		var classes = cname.trim().split( " " );
 		return this.each(function(){
 			for( var i = 0, il = classes.length; i < il; i++ ){
-				if( !this.className.match( new RegExp( "(^|\\s)" + classes[ i ] + "($|\\s)" ) ) ){
+				if( this.className !== undefined && ( this.className === "" || !this.className.match( new RegExp( "(^|\\s)" + classes[ i ] + "($|\\s)" ) ) ) ){
 					this.className += " " + classes[ i ];
 				}
 			}
