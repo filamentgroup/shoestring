adfb00e294ed7e370a7a89d9b48a775c8b60ec3c
Scott Jehl <scott@scottjehl.com>
2012-07-25 11:06:05 -0400

    improved removeClass method to be more strict in what it removes (not pieces of a class that happen to match). It doesn't clean whitespace, though. Sorry.

--- src/extensions/wrap.dom.removeClass.js
+++ src/extensions/wrap.dom.removeClass.js
@@ -1,11 +1,11 @@
 // Extensions
 (function( undefined ){
 	wrap.fn.removeClass = function( cname ){
-		var classes = cname.split( " " );
+		var classes = cname.trim().split( " " );
 		
 		return this.each(function(){
 			for( var i = 0, il = classes.length; i < il; i++ ){
-				this.className = this.className.replace( new RegExp( classes[ i ] + "[$\s]*", "gmi" ), "" );
+				this.className = this.className.replace( new RegExp( "(^|\\s)" + classes[ i ] + "($|\\s)", "gmi" ), " " );
 			}
 		});
 	};
