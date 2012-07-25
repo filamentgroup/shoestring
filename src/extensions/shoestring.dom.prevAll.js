bbf791f8d4180f4ef58ac6b6d23d6a274414faa5
Scott Jehl <scott@scottjehl.com>
2012-07-25 15:26:01 -0400

    this makes prevAll not do an infinite loop in cases where it used to. If you miss that feature, try dividing by zero or whatever.

--- src/extensions/shoestring.dom.prevAll.js
+++ src/extensions/shoestring.dom.prevAll.js
@@ -3,11 +3,12 @@
 // keep this wrapper around the ones you use!
 (function( undefined ){
 	shoestring.fn.prevAll = function(){
-		var ret = [],
-			next;
+		var ret = [];
 		this.each(function( i ){
-			while( this.previousElementSibling ){
-				ret = ret.concat( this.previousElementSibling );
+			var self = this;
+			while( self.previousElementSibling ){
+				ret = ret.concat( self.previousElementSibling );
+				self = self.previousElementSibling;
 			}
 		});
 		return shoestring(ret);
