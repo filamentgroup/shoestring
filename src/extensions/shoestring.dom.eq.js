cb9afb80aeeaf963ef6074e6a86a810126148228
Scott Jehl <scott@scottjehl.com>
2012-07-25 15:44:19 -0400

    eq method should return an empty array if passed a negative number

--- src/extensions/shoestring.dom.eq.js
+++ src/extensions/shoestring.dom.eq.js
@@ -3,6 +3,9 @@
 // keep this wrapper around the ones you use!
 (function( undefined ){
 	shoestring.fn.eq = function( num ){
+		if( num < 0 ){
+			return shoestring([]);
+		}
 		return shoestring( this[ num ] );
 	};
 }());
