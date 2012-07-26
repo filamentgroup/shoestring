fa612c23740ad901e4a4c952651a745fd4ee83c9
Scott Jehl <scott@scottjehl.com>
2012-07-26 19:09:14 -0400

    improved the live method so that live events can be bound to elements not yet present in the DOM.

--- src/extensions/shoestring.events.live.js
+++ src/extensions/shoestring.events.live.js
@@ -3,24 +3,25 @@
 // keep this shoestringper around the ones you use!
 (function( undefined ){
 	shoestring.fn.live = function( evt, callback ){
-		var evts = evt.split( " " );
-		return this.each(function(){
-			var self = this;
-			
-			function newCB( e ){
-				if( event.target === self ){
+		var evts = evt.split( " " ),
+			sel = this.selector;
+		
+		function newCB( e ){
+			shoestring( sel ).each(function(){
+				if( e.target === this ){
 					callback.call( self, e );
 				}
+			});
+		}
+		
+		for( var i = 0, il = evts.length; i < il; i++ ){
+			if( "addEventListener" in document ){
+				document.addEventListener( evts[ i ], newCB, false );
 			}
-			
-			for( var i = 0, il = evts.length; i < il; i++ ){
-				if( "addEventListener" in document ){
-					document.addEventListener( evts[ i ], newCB, false );
-				}
-				else if( document.attachEvent ){
-					document.attachEvent( "on" + evts[ i ], newCB );
-				}
+			else if( document.attachEvent ){
+				document.attachEvent( "on" + evts[ i ], newCB );
 			}
-		});
+		}
+		return this;
 	};
 }());
