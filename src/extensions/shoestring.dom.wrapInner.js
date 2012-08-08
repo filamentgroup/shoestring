c309bac53011d4f58148d15da80353ba1c85ad4d
Scott Jehl <scott@scottjehl.com>
2012-08-08 15:47:37 -0500

    shoestringInner is a bad method name.

--- src/extensions/shoestring.dom.wrapInner.js
+++ src/extensions/shoestring.dom.wrapInner.js
@@ -2,7 +2,7 @@
 
 // keep this wrapper around the ones you use!
 (function( undefined ){
-	shoestring.fn.shoestringInner = function( html ){
+	shoestring.fn.wrapInner = function( html ){
 		return this.each(function(){
 			var inH = this.innerHTML;
 			this.innerHTML = "";
