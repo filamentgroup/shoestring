c141ecf0e42d553f9400d5f1b97907fa4b443a43
Alexander Schmitz <arschmitz@gmail.com>
2013-03-25 11:17:32 -0400

    Add $.each and $.merge

---
+++ src/extensions/shoestring.util.merge.js
@@ -0,0 +1,24 @@
+// Extensions
+
+// keep this wrapper around the ones you use!
+(function( undefined ){
+	shoestring.merge = function( first, second ){
+		var l = second.length,
+			i = first.length,
+			j = 0;
+
+		if ( typeof l === "number" ) {
+			for ( ; j < l; j++ ) {
+				first[ i++ ] = second[ j ];
+			}
+		} else {
+			while ( second[j] !== undefined ) {
+				first[ i++ ] = second[ j++ ];
+			}
+		}
+
+		first.length = i;
+
+		return shoestring( first );
+	};
+}());
