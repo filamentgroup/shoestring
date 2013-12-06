f1b849b6b2d2ca60d884063355c4070399fbb272
Scott Jehl <scott@scottjehl.com>
2013-12-06 14:35:15 -0600

    improved ajax response success/error/cancel

--- src/extensions/shoestring.ajax.ajax.js
+++ src/extensions/shoestring.ajax.ajax.js
@@ -25,7 +25,7 @@
 		
 		if( !req || !url ){
 			return;
-		}	
+		}
 		
 		req.open( settings.method, url, settings.async );
 
@@ -34,25 +34,33 @@
 		}
 		
 		req.onreadystatechange = function () {
-			// Trim the whitespace so shoestring('<div>') works
-			var res = (req.responseText || '').replace(/^\s+|\s+$/g, '');
-			if ( req.status.toString().match( /^(4|5)/ ) && RegExp.$1 ){
-				return settings.error( res, req.status, req );
-			}
-			else {
-				return settings.success( res, req.status, req );
+			if( req.readyState === 4 ){
+				// Trim the whitespace so shoestring('<div>') works
+				var res = (req.responseText || '').replace(/^\s+|\s+$/g, '');
+				if( req.status.toString().indexOf( "0" ) === 0 ){
+					return settings.cancel( res, req.status, req );
+				}
+				else if ( req.status.toString().match( /^(4|5)/ ) && RegExp.$1 ){
+					return settings.error( res, req.status, req );
+				}
+				else {
+					return settings.success( res, req.status, req );
+				}
 			}
 		};
+
 		if( req.readyState === 4 ){
-			return;
+			return req;
 		}
 
 		req.send( null );
+		return req;
 	};
 	
 	shoestring.ajax.settings = {
 		success: function(){},
 		error: function(){},
+		cancel: function(){},
 		method: "GET",
 		async: true,
 		data: null
