b08d0283a51180bdc6bedd5855a2392cf7072ab0
Zach Leatherman <zachleatherman@gmail.com>
2014-01-09 13:29:14 -0600

    insertAfter would sometimes replace the sibling node

--- src/extensions/shoestring.dom.after.js
+++ src/extensions/shoestring.dom.after.js
@@ -7,8 +7,7 @@
 		return this.each(function( i ){
 			for( var j = 0, jl = frag.length; j < jl; j++ ){
 				var insertEl = i > 0 ? frag[ j ].cloneNode( true ) : frag[ j ];
-				this.parentNode.insertBefore( insertEl, this );
-				this.parentNode.insertBefore( this, insertEl );
+				this.parentNode.insertBefore( insertEl, this.nextSibling );
 			}
 		});
 	};
