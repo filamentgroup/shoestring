3fa87469017f5acba0f8d441e19b02ce82ec77e4
Zach Leatherman <zachleatherman@gmail.com>
2014-01-09 12:17:35 -0600

    Simple tests for closest, insertAfter, insertBefore

--- test/unit/core.js
+++ test/unit/core.js
@@ -147,4 +147,39 @@ test( '`.live()` is an alias of `.on()`', function() {
 	ok( shoestring( "body" ).live == shoestring( "body" ).on );
 });
 
+test( '`.closest()`', function() {
+	var $fixture = shoestring( '#qunit-fixture' );
+	$fixture.html( '<div class="parent second"><div class="parent first"><div class="child"></div></div></div>' );
+
+	equal( $fixture.find( '.child' ).closest( '.parent' ).length, 1, 'Closest returns only one element.' );
+
+	ok( $fixture.find( '.child' ).closest( '.parent' ).is( '.first' ), 'Closest returns from the bottom up.' );
+});
+
+test( '`.insertAfter()`', function() {
+	var $fixture = shoestring( '#qunit-fixture' ),
+		$sibling;
+
+	$fixture.html( '<div id="sibling"></div>' );
+	$sibling = shoestring( '#sibling' );
+
+	shoestring( '<div id="insertAfter">' ).insertAfter( $sibling );
+
+	equal( $fixture.children().length, 2, 'Has two children.' );
+	ok( $sibling.next().is( '#insertAfter' ), 'Inserted properly.' );
+});
+
+test( '`.insertBefore()`', function() {
+	var $fixture = shoestring( '#qunit-fixture' ),
+		$sibling;
+
+	$fixture.html( '<div id="sibling"></div>' );
+	$sibling = shoestring( '#sibling' );
+
+	shoestring( '<div id="insertBefore">' ).insertBefore( $sibling );
+
+	equal( $fixture.children().length, 2, 'Has two children.' );
+	ok( $sibling.prev().is( '#insertBefore' ), 'Inserted properly.' );
+});
+
 })();
