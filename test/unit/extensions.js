1d30c9a4b2bad7b3864db3c7724fa24624c130b3
John Bender <john.m.bender@gmail.com>
2014-05-27 10:16:31 -0700

    remove redundant tests

--- test/unit/extensions.js
+++ test/unit/extensions.js
@@ -134,11 +134,6 @@
 		equal( $clone.attr( "class" ), "foo" );
 	});
 
-
-	test( '`.live()` is an alias of `.on()`', function() {
-		ok( shoestring( "body" ).live == shoestring( "body" ).on );
-	});
-
 	test( '`.closest()`', function() {
 		var $fixture = shoestring( '#qunit-fixture' );
 		$fixture.html( '<div class="parent second"><div class="parent first"><div class="child"></div></div></div>' );
@@ -152,33 +147,6 @@
 		ok( $child.closest( '.parent' ).is( '.first' ), 'Closest returns from the bottom up.' );
 	});
 
-	test( '`.insertAfter()`', function() {
-		var $fixture = shoestring( '#qunit-fixture' ),
-				$sibling;
-
-		$fixture.html( '<div id="sibling"></div>' );
-		$sibling = shoestring( '#sibling' );
-
-		shoestring( '<div id="insertAfter">' ).insertAfter( $sibling );
-
-		equal( $fixture.children().length, 2, 'Has two children.' );
-		ok( $sibling.next().is( '#insertAfter' ), 'Inserted properly.' );
-	});
-
-	test( '`.insertBefore()`', function() {
-		var $fixture = shoestring( '#qunit-fixture' ),
-				$sibling;
-
-		$fixture.html( '<div id="sibling"></div>' );
-		$sibling = shoestring( '#sibling' );
-
-		shoestring( '<div id="insertBefore">' ).insertBefore( $sibling );
-
-		equal( $fixture.children().length, 2, 'Has two children.' );
-		ok( $sibling.prev().is( '#insertBefore' ), 'Inserted properly.' );
-	});
-
-
 	test( '`.html()`', function() {
 		var $fixture = shoestring( '#qunit-fixture' ),
 				htmlStr = '<div id="sibling"></div>';
@@ -186,6 +154,10 @@
 		$fixture.html( htmlStr );
 		equal( $fixture[0].innerHTML, htmlStr, '.html(str) set properly.' );
 		equal( $fixture.html(), htmlStr, '.html() get properly.' );
+	});
+
+	test( '`.live()` is an alias of `.on()`', function() {
+		ok( shoestring( "body" ).live == shoestring( "body" ).on );
 	});
 
 	test( '`.siblings()`', function() {
