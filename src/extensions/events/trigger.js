9805c2523cfad2427c3e8c1ad0e9573d1ddfcfbe
Zach Leatherman <zachleatherman@gmail.com>
2014-07-10 16:31:12 -0500

    N/A comment

--- src/extensions/events/trigger.js
+++ src/extensions/events/trigger.js
@@ -6,7 +6,6 @@ define([ "shoestring" ], function(){
 		var evts = evt.split( " " );
 		return this.each(function(){
 			for( var i = 0, il = evts.length; i < il; i++ ){
-				// TODO needs IE8 support
 				if( document.createEvent ){
 					var event = document.createEvent( "Event" );
 					event.initEvent( evts[ i ], true, true );
