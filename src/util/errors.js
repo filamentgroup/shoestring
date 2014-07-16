5914f777440ca7b01cdd60c0b8ab43ff4aa218ca
John Bender <john.m.bender@gmail.com>
2014-07-16 10:21:38 -0700

    dev more errors util whitespace

--- src/util/errors.js
+++ src/util/errors.js
@@ -2,15 +2,15 @@
 define([ "shoestring" ], function(){
 //>>excludeEnd("exclude");
 
-  shoestring.enUS = {
-    errors: {
-      'prev-selector' : "Shoestring does not support passing selectors into .prev, try .prev().filter(selector)"
-    }
-  };
+	shoestring.enUS = {
+		errors: {
+			'prev-selector' : "Shoestring does not support passing selectors into .prev, try .prev().filter(selector)"
+		}
+	};
 
-  shoestring.error = function( id ) {
-    throw new Error( shoestring.enUS.errors[id] );
-  };
+	shoestring.error = function( id ) {
+		throw new Error( shoestring.enUS.errors[id] );
+	};
 
 //>>excludeStart("exclude", pragmas.exclude);
 });
