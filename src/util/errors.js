df4581137926a9bae7526a471a5fab40399096b5
Jeffrey Lembeck <jlembeck@gmail.com>
2014-07-16 11:29:53 -0700

    This project uses tabs not spaces

--- src/util/errors.js
+++ src/util/errors.js
@@ -2,16 +2,16 @@
 define([ "shoestring" ], function(){
 //>>excludeEnd("exclude");
 
-  shoestring.enUS = {
-    errors: {
-      'prev-selector' : "Shoestring does not support passing selectors into .prev, try .prev().filter(selector)",
+	shoestring.enUS = {
+		errors: {
+			'prev-selector' : "Shoestring does not support passing selectors into .prev, try .prev().filter(selector)",
 			'hasClass' : "Shoestring does not support the hasClass method. Try using .is( '.klassname' ) instead"
-    }
-  };
+		}
+	};
 
-  shoestring.error = function( id ) {
-    throw new Error( shoestring.enUS.errors[id] );
-  };
+	shoestring.error = function( id ) {
+		throw new Error( shoestring.enUS.errors[id] );
+	};
 
 //>>excludeStart("exclude", pragmas.exclude);
 });
