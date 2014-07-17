b8e3693760d7447c90410a184450c03e3d61385a
John Bender <john.m.bender@gmail.com>
2014-07-17 13:20:16 -0700

    dep on index, next works everywhere

--- src/extensions/dom/next.js
+++ src/extensions/dom/next.js
@@ -1,25 +1,33 @@
 //>>excludeStart("exclude", pragmas.exclude);
-define([ "shoestring" ], function(){
+define([ "shoestring", "extensions/dom/index" ], function(){
 //>>excludeEnd("exclude");
 
 	shoestring.fn.next = function(){
-		var ret = [],
-			next;
-
 		//>>includeStart("development", pragmas.development);
 		if( arguments.length > 0 ){
 			shoestring.error( 'next-selector' );
 		}
 		//>>includeEnd("development");
 
-		this.each(function(){
-			next = this.nextElementSibling;
+		var result = [];
 
-			if( next ){
-				ret = ret.concat( next );
+		// TODO need to implement map
+		this.each(function() {
+			var children, item;
+
+			// get the child nodes for this member of the set
+			children = shoestring( this.parentNode )[0].childNodes;
+
+			// grab the next element based on the current element's index
+			item = children.item( shoestring( this ).index() + 1 );
+
+			// push the item onto the result set if it exists
+			if( item ) {
+				result.push( item );
 			}
 		});
-		return shoestring(ret);
+
+		return shoestring( result );
 	};
 
 //>>excludeStart("exclude", pragmas.exclude);
