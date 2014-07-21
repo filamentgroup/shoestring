396f3b73c106638fdfef0568684d691f1543a49a
John Bender <john.m.bender@gmail.com>
2014-07-21 10:11:26 -0700

    whitespace

--- src/extensions/dom/prevAll.js
+++ src/extensions/dom/prevAll.js
@@ -4,13 +4,16 @@ define([ "shoestring" ], function(){
 
 	shoestring.fn.prevAll = function(){
 		var ret = [];
+
 		this.each(function(){
 			var self = this;
+
 			while( self.previousElementSibling ){
 				ret = ret.concat( self.previousElementSibling );
 				self = self.previousElementSibling;
 			}
 		});
+
 		return shoestring(ret);
 	};
 
