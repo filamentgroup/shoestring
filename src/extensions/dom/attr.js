4c567d92e88740aac923a178a89b2991b8298d9b
John Bender <john.m.bender@gmail.com>
2014-08-18 13:05:42 -0400

    docs for attr

--- src/extensions/dom/attr.js
+++ src/extensions/dom/attr.js
@@ -2,14 +2,22 @@
 define([ "shoestring" ], function(){
 //>>excludeEnd("exclude");
 
-	shoestring.fn.attr = function( name, val ){
+  /**
+	 * Get the value of the first element of the set or set the value of all the elements in the set.
+	 *
+	 * @param {string} name The attribute name.
+	 * @param {string} value The new value for the attribute.
+	 * @return {shoestring|string|undefined}
+	 * @this {shoestring}
+	 */
+	shoestring.fn.attr = function( name, value ){
 		var nameStr = typeof( name ) === "string";
-		if( val !== undefined || !nameStr ){
+
+		if( value !== undefined || !nameStr ){
 			return this.each(function(){
 				if( nameStr ){
-					this.setAttribute( name, val );
-				}
-				else {
+					this.setAttribute( name, value );
+				}	else {
 					for( var i in name ){
 						if( name.hasOwnProperty( i ) ){
 							this.setAttribute( i, name[ i ] );
@@ -17,8 +25,7 @@ define([ "shoestring" ], function(){
 					}
 				}
 			});
-		}
-		else {
+		} else {
 			return this[ 0 ] ? this[ 0 ].getAttribute( name ) : undefined;
 		}
 	};
