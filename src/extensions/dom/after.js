74cbc4b8787958aa1d4c31d67a7762afba93400e
John Bender <john.m.bender@gmail.com>
2014-08-18 12:34:32 -0400

    change param names, documentation

--- src/extensions/dom/after.js
+++ src/extensions/dom/after.js
@@ -2,21 +2,37 @@
 define([ "shoestring" ], function(){
 //>>excludeEnd("exclude");
 
-	shoestring.fn.after = function( frag ){
-		if( typeof( frag ) === "string" || frag.nodeType !== undefined ){
-			frag = shoestring( frag );
+
+	/**
+	 * Insert an element or HTML string after each element in the current set.
+	 *
+	 * @param {string|HTMLElement} fragment The HTML or HTMLElement to insert.
+	 * @return shoestring
+	 * @this shoestring
+	 */
+	shoestring.fn.after = function( fragment ){
+		if( typeof( fragment ) === "string" || fragment.nodeType !== undefined ){
+			fragment = shoestring( fragment );
 		}
+
 		return this.each(function( i ){
-			for( var j = 0, jl = frag.length; j < jl; j++ ){
-				var insertEl = i > 0 ? frag[ j ].cloneNode( true ) : frag[ j ];
+			for( var j = 0, jl = fragment.length; j < jl; j++ ){
+				var insertEl = i > 0 ? fragment[ j ].cloneNode( true ) : fragment[ j ];
 				this.parentNode.insertBefore( insertEl, this.nextSibling );
 			}
 		});
 	};
 
-	shoestring.fn.insertAfter = function( sel ){
+	/**
+	 * Insert the current set after the elements matching the selector.
+	 *
+	 * @param {string} selector The selector after which to inser the current set.
+	 * @return shoestring
+	 * @this shoestring
+	 */
+	shoestring.fn.insertAfter = function( selector ){
 		return this.each(function(){
-			shoestring( sel ).after( this );
+			shoestring( selector ).after( this );
 		});
 	};
 
