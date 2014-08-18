b4cfc265713b37b3e1f032d51a7e2d5857f4f5aa
John Bender <john.m.bender@gmail.com>
2014-08-18 12:39:58 -0400

    typo

--- src/extensions/dom/insertAfter.js
+++ src/extensions/dom/insertAfter.js
@@ -5,7 +5,7 @@ define([ "shoestring", "extensions/dom/after" ], function(){
 	/**
 	 * Insert the current set after the elements matching the selector.
 	 *
-	 * @param {string} selector The selector after which to inser the current set.
+	 * @param {string} selector The selector after which to insert the current set.
 	 * @return shoestring
 	 * @this shoestring
 	 */
