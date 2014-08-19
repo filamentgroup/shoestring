992afa2df96af0e440b16aa70cb596043a9e21a4
John Bender <john.m.bender@gmail.com>
2014-08-19 12:19:24 -0400

    doc eq

--- src/dom/clone.js
+++ src/dom/clone.js
@@ -3,7 +3,7 @@ define([ "shoestring" ], function(){
 //>>excludeEnd("exclude");
 
 	/**
-	 * Clone and return the current set of nodes into a new ShoeString object.
+	 * Clone and return the current set of nodes into a new `shoestring` object.
 	 *
 	 * @return shoestring
 	 * @this shoestring
