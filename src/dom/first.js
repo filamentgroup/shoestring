78dcec65a5ed763ec676a6e6e0b2b1db10849e16
John Bender <john.m.bender@gmail.com>
2014-08-19 12:34:20 -0400

    doc first add eq dep

--- src/dom/first.js
+++ src/dom/first.js
@@ -1,9 +1,17 @@
 //>>excludeStart("exclude", pragmas.exclude);
-define([ "shoestring" ], function(){
+define([ "shoestring", "dom/eq" ], function(){
 //>>excludeEnd("exclude");
-  shoestring.fn.first = function(){
+
+	/**
+	 * Returns the first element of the set wrapped in a new `shoestring` object.
+	 *
+	 * @return shoestring
+	 * @this shoestring
+	 */
+	shoestring.fn.first = function(){
 		return this.eq( 0 );
 	};
+
 //>>excludeStart("exclude", pragmas.exclude);
 });
 //>>excludeEnd("exclude");
