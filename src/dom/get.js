866b07760fb91e395e34d7d8ad12468a0ab9afc7
John Bender <john.m.bender@gmail.com>
2014-08-19 12:36:09 -0400

    doc dom/get, change param name

--- src/dom/get.js
+++ src/dom/get.js
@@ -2,8 +2,15 @@
 define([ "shoestring" ], function(){
 //>>excludeEnd("exclude");
 
-	shoestring.fn.get = function( num ){
-		return this[ num ];
+	/**
+	 * Returns the raw DOM node at the passed index.
+	 *
+	 * @param {integer} index The index of the element to wrap and return.
+	 * @return HTMLElement
+	 * @this shoestring
+	 */
+	shoestring.fn.get = function( index ){
+		return this[ index ];
 	};
 
 //>>excludeStart("exclude", pragmas.exclude);
