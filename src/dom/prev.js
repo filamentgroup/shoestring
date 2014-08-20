8157a4ab8756f8f4cd8aa4992a1b6d12155da7e8
John Bender <john.m.bender@gmail.com>
2014-08-20 10:31:31 -0400

    doc prev

--- src/dom/prev.js
+++ src/dom/prev.js
@@ -2,6 +2,12 @@
 define([ "shoestring" ], function(){
 //>>excludeEnd("exclude");
 
+	/**
+	 * Returns a `shoestring` object with the set of siblings before each element in the original set.
+	 *
+	 * @return shoestring
+	 * @this shoestring
+	 */
 	shoestring.fn.prev = function(){
 		//>>includeStart("development", pragmas.development);
 		if( arguments.length > 0 ){
