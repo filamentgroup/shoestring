31e4f2a7401bbebbe7fe0dbda75eb80db45f9d5b
John Bender <john.m.bender@gmail.com>
2014-08-20 13:42:21 -0400

    doc data

--- src/data/data.js
+++ src/data/data.js
@@ -1,25 +1,32 @@
-// Extensions
-
-// keep this wrapper around the ones you use!
 //>>excludeStart("exclude", pragmas.exclude);
 define([ "shoestring" ], function(){
 //>>excludeEnd("exclude");
-	shoestring.fn.data = function( name, val ){
+
+	/**
+	 * Get data attached to the first element or set data values on all elements in the current set.
+	 *
+	 * @param {string} name The data attribute name.
+	 * @param {any} value The value assigned to the data attribute.
+	 * @return shoestring
+	 * @this shoestring
+	 */
+	shoestring.fn.data = function( name, value ){
 		if( name !== undefined ){
-			if( val !== undefined ){
+			if( value !== undefined ){
 				return this.each(function(){
 					if( !this.shoestringData ){
 						this.shoestringData = {};
 					}
-					this.shoestringData[ name ] = val;
+
+					this.shoestringData[ name ] = value;
 				});
 			}
 			else {
-				return this.length && this[ 0 ].shoestringData ? this[ 0 ].shoestringData[ name ] : undefined;
+				return this[ 0 ] && this[ 0 ].shoestringData ? this[ 0 ].shoestringData[ name ] : undefined;
 			}
 		}
 		else {
-			return this.length ? this[ 0 ].shoestringData || {} : undefined;
+			return this[ 0 ] ? this[ 0 ].shoestringData || {} : undefined;
 		}
 	};
 //>>excludeStart("exclude", pragmas.exclude);
