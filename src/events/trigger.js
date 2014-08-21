538d67eeb9534146b3d268ebb0e72f89e41efc7a
John Bender <john.m.bender@gmail.com>
2014-08-21 14:38:09 -0400

    whitespace

--- src/events/trigger.js
+++ src/events/trigger.js
@@ -4,12 +4,14 @@ define([ "shoestring" ], function(){
 
 	shoestring.fn.trigger = function( evt, args ){
 		var evts = evt.split( " " );
+
 		return this.each(function(){
 			for( var i = 0, il = evts.length; i < il; i++ ){
 				if( document.createEvent ){
 					var event = document.createEvent( "Event" );
 					event.initEvent( evts[ i ], true, true );
 					event._args = args;
+
 					this.dispatchEvent( event );
 				} else if ( document.createEventObject ) {
 					if( ( "" + this[ evts[ i ] ] ).indexOf( "function" ) > -1 ) {
