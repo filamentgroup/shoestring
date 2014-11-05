94c7dcdbea46e06bbc96c90f5d49468f5c928058
John Bender <john.m.bender@gmail.com>
2014-11-05 00:42:42 -0500

    whitespace

--- src/shoestring.js
+++ src/shoestring.js
@@ -54,13 +54,13 @@ define([], function(){
 				for( var i = 0, il = sel.length; i < il; i++ ){
 					ret[ i ] = sel[ i ];
 				}
-			}	else if( Object.prototype.toString.call( pType ) === '[object Array]' ||
+			} else if( Object.prototype.toString.call( pType ) === '[object Array]' ||
 		             pType === "object" && prim instanceof w.NodeList ){
 
 			  for( var i2 = 0, il2 = prim.length; i2 < il2; i2++ ){
 					ret[ i2 ] = prim[ i2 ];
 				}
-		  }	else {
+		  } else {
 				// object? passthrough
 				// TODO handle concat of shoestring objects
 				ret = ret.concat( prim );
